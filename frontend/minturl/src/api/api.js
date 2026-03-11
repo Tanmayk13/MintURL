35413ac4511c5948a58de81663b04d981a3f4056
Tanmay Khilari <khilaritanmay24@gmail.com>
2026-03-11 20:12:35 +0530

    Updated base backend URL in frontend

--- frontend/minturl/src/api/api.js
+++ frontend/minturl/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://minturl-backend.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://minturl.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
